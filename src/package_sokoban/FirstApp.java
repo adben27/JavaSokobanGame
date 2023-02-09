7ada7522728e55d7ae684c4994d30b6e0dada3d3
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-02-09 21:46:08 +0100

    FirstApp est la classe Main du jeu

--- src/package_sokoban/FirstApp.java
+++ src/package_sokoban/FirstApp.java
@@ -10,7 +10,6 @@ import java.util.Random;
 //classe FirstApp qui est une JFrame et qui implement l'interface KeyListener
 public class FirstApp extends JFrame implements KeyListener{
     private static final int WIDTH=600, HEIGHT=400; //hauteur et largeur de la fenetre
-
     private JButton haut, bas, gauche, droite;//boutton qui permettront de ce deplacer si on a la flemme d'utiliser les fleches directionelles
     //private Img joueur;//les differentes images qu'on va utiliser
     private DrawLevel niveau;//le conteneur ou il y aura le niveau largeur=420 (21x20) longueur=320 (16x20)
@@ -26,9 +25,9 @@ public class FirstApp extends JFrame implements KeyListener{
         //on dit a la fenetre d'écouter le clavier
         addKeyListener(this);
 
-        setResizable(false);
+        //setResizable(false);
 
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //on ferme le processus de la fenetre quand on clique sur la croix rouge
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //on ferme le processus de la fenetre quand on clique sur la croix rouge
         
         setSize(WIDTH, HEIGHT); //la fenetre fera WIDHT pixels de hauteur et HEIGHT de largeur
         
@@ -41,6 +40,7 @@ public class FirstApp extends JFrame implements KeyListener{
         //on cree le JPanel qui contiendra le niveau et on l'ajoute dans la JFrame
         niveau= new DrawLevel();
         niveau.setLayout(null);
+        add(niveau, BorderLayout.CENTER);
         
         //on genere 3 nombres aleatoires qui feront la couleur du niveau
         Random random = new Random();
@@ -49,12 +49,8 @@ public class FirstApp extends JFrame implements KeyListener{
         b=random.nextInt(256);
         niveau.setBackground(new Color(r, g, b));
 
-        add(niveau, BorderLayout.CENTER);
-
         //on cree l'images du joueur qui va etre utiliser (255,91)
-        /*joueur = new Img("Image/joueur.png");
-        joueur.setLocation(((niveau.getWidth() - joueur.getWidth())/2) , ((niveau.getHeight() - joueur.getHeight())/2) );
-        */
+       
         //on creeé les bouton "haut" "bas" "gauche" "droite" et grace à "conteneurMove" on ajoute ces 4 button dans le conteneur qui va se trouver en bas de la fenetre
         droite = new JButton("droite");
         gauche = new JButton("gauche");
@@ -65,47 +61,30 @@ public class FirstApp extends JFrame implements KeyListener{
 
         add(information(), BorderLayout.WEST); //on rajoute des informations a gauche de la fenetre
 
-        //niveau.add(joueur);
-
         setVisible(true); //On affiche la fenetre 
+
+        niveau.startGame();
     }
 
-    //les Listener sont les methodes qui appeleront la methode move et appliqueront les modifications qui suivent (KetListener pour le clavier et Listener pour les bouttons)
-    //les println seront remplacer par les mouvements que le joueur fera
-    private void hautKeyListener() {
-        niveau.getJoueur().deplacementHaut();
-    }
-
-    private void basKeyListener() {
-        niveau.getJoueur().deplacementBas();
-    }
-
-    private void gaucheKeyListener() {
-        niveau.getJoueur().deplacementGauche();
-    }
-
-    private void droiteKeyListener() {
-        niveau.getJoueur().deplacementDroite();
-    }
-
+    //les Listener sont les methodes qui appeleront la methode move et appliqueront les modifications qui suivent
     //requestFocus() permette d'ecouter le clavier, sans les requestFocus() le clavier n'est plus ecouter 
     private void hautListener(ActionEvent e) {
-        niveau.getJoueur().deplacementHaut();
-        requestFocus(); 
+        niveau.setHaut(true);
+        requestFocus();
     }
 
     private void droiteListener(ActionEvent e) {
-        niveau.getJoueur().deplacementDroite();
+        niveau.setDroite(true);
         requestFocus();
     }
 
     private void gaucheListener(ActionEvent e) {
-        niveau.getJoueur().deplacementGauche();
+        niveau.setGauche(true);
         requestFocus();
     }
 
     private void basListener(ActionEvent e) {
-        niveau.getJoueur().deplacementBas();
+        niveau.setBas(true);
         requestFocus();
     }
 
@@ -113,30 +92,43 @@ public class FirstApp extends JFrame implements KeyListener{
     public void keyTyped(KeyEvent e) { }
 
     @Override
-    public void keyReleased(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_UP) {
+            niveau.setHaut(false);
+        }
+        if (keyCode == KeyEvent.VK_DOWN) {
+            niveau.setBas(false);
+        }
+        if (keyCode == KeyEvent.VK_LEFT) {
+            niveau.setGauche(false);
+        }
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            niveau.setDroite(false);
+        }
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
 
         if (keyCode == KeyEvent.VK_UP) {
-            hautKeyListener();
-            return;
+            niveau.setHaut(true);
         }
         if (keyCode == KeyEvent.VK_DOWN) {
-            basKeyListener();
-            return;
+            niveau.setBas(true);
         }
         if (keyCode == KeyEvent.VK_LEFT) {
-            gaucheKeyListener();
-            return;
+            niveau.setGauche(true);
         }
         if (keyCode == KeyEvent.VK_RIGHT) {
-            droiteKeyListener();
-            return;
+            niveau.setDroite(true);
         }
-        JOptionPane.showMessageDialog(this,"Seuls les flèches directionnelles peuvent être utilisé\n(Si vous ne voulez pas utiliser les flèches directionnelles\nutilisez les bouttons haut, bas, gauche, droite)","Information", JOptionPane.INFORMATION_MESSAGE);
-    } 
+        if (keyCode != KeyEvent.VK_RIGHT && keyCode != KeyEvent.VK_LEFT && keyCode != KeyEvent.VK_DOWN && keyCode != KeyEvent.VK_UP) {
+            JOptionPane.showMessageDialog(this,"Seuls les flèches directionnelles peuvent être utilisé\n(Si vous ne voulez pas utiliser les flèches directionnelles\nutilisez les bouttons haut, bas, gauche, droite)","Information", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
 
     private JPanel createConteneurMove(JButton haut, JButton bas, JButton gauche, JButton droite){
         //creation des panel ou il y aura les mouvements
@@ -206,7 +198,7 @@ public class FirstApp extends JFrame implements KeyListener{
         info.add(new Img("Image/mur.png"));//on met l'image a gauche du panel
 
         return info;
-    }
+    }    
 
     public static void main(String[] args) throws Exception{
         //la fenetre aura le look Nimbus
@@ -214,5 +206,5 @@ public class FirstApp extends JFrame implements KeyListener{
 
         //creation de la fenetre
         new FirstApp(); 
-    }    
+    }
 }
