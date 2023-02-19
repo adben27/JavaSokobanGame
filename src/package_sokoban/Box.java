e3bdc703804591876ce750e895df6916c5b852ee
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-02-19 14:17:51 +0100

    Rajout du parametre name

--- src/package_sokoban/Box.java
+++ src/package_sokoban/Box.java
@@ -1,9 +1,9 @@
 package package_sokoban;
 
-public class Vide extends Element{
-    
-    public Vide(boolean on_target) {
-        super(' ',true,on_target,'@');
+public class Box extends Element{
+	
+    public Box(boolean on_target, char name) {
+        super(name,true,on_target, (char)(name + 32));
         this.on_target=on_target;
         super.setOn_target(on_target);
     }
