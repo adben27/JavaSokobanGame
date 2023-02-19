654963212016d2e6e51efd7b04db9f9e6c572b63
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-02-19 13:27:54 +0100

    Remplcer 'X' par '@'

--- src/package_sokoban/Vide.java
+++ src/package_sokoban/Vide.java
@@ -3,7 +3,7 @@ package package_sokoban;
 public class Vide extends Element{
     
     public Vide(boolean on_target) {
-        super(' ',true,on_target,'X');
+        super(' ',true,on_target,'@');
         this.on_target=on_target;
         super.setOn_target(on_target);
     }
