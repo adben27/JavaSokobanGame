29940c95639249a7a38a2bb2126ebe222ae4cc1a
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2023-03-16 18:49:36 +0000

    Replace Box.java

--- package_sokoban/Box.java
+++ package_sokoban/Box.java
@@ -2,6 +2,12 @@ package package_sokoban;
 
 public class Box extends Element{
 	
+    public Box(boolean on_target, char sign) {
+        super(sign,true,on_target, (char) (sign+32));
+        this.on_target=on_target;
+        super.setOn_target(on_target);
+    }
+
     public Box(boolean on_target) {
         super('B',true,on_target, 'b');
         this.on_target=on_target;
