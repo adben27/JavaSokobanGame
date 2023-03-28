7ff7b6ef2179b73ba7b743d38b7d5038e8efeb5d
Nandan NAGARADJE <nandan.nagaradje@edu.univ-paris13.fr>
2023-03-28 11:10:37 +0000

    Replace Matrice.java

--- package_sokoban/Matrice.java
+++ package_sokoban/Matrice.java
@@ -72,7 +72,7 @@ public class Matrice extends Element{
     
     //  constructeur qui initialise pos_x et pos_y a 0 utilisé par nandan pour la lecture de niveau
     public Matrice(String name, char sign , boolean on_target, int size){
-        super(Character.toUpperCase(sign),true,on_target,sign);// On donne au constructeur un signe en majuscule mais l'affichage de base sera en minuscule et sur la cible sera en majuscule
+        super(Character.toUpperCase(sign),true,on_target,Character.toLowerCase(sign));// On donne au constructeur un signe en majuscule mais l'affichage de base sera en minuscule et sur la cible sera en majuscule
     	this.name=name;
         this.size=size;
         this.pos_x=pos_x_copie=0;
